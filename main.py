346c54158994c06bef5e8ceba0303f172bda39b3
Hmcruz02 <hmcruz02@gmail.com>
2022-07-26 19:50:29 -0500

    Added reload command

--- main.py
+++ main.py
@@ -7,13 +7,21 @@ load_dotenv()
 
 bot = commands.Bot(command_prefix="!")
 
+#loads in an extension
 @bot.command()
 async def load(ctx, extension):
     bot.load_extension(f'cogs.{extension}')
 
+#unloads an extension
 @bot.command()
 async def unload(ctx, extension):
     bot.unload_extension(f'cogs.{extension}')
+
+#reloads an extension
+@bot.command()
+async def reload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
+    bot.load_extension(f'cogs.{extension}')
 
 for filename in os.listdir('./BunnyBot/cogs'):
     if filename.endswith('.py'):
