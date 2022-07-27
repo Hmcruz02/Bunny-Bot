c99b88678aedf2b065d9f9d1cc1bdbd003810986
Hmcruz02 <hmcruz02@gmail.com>
2022-07-27 18:52:28 -0500

    Made custom help command

--- main.py
+++ main.py
@@ -5,7 +5,32 @@ from dotenv import load_dotenv
 from os import getenv
 load_dotenv()
 
-bot = commands.Bot(command_prefix="!")
+#This class is to create and embedded help command
+class MyHelpCommand(commands.HelpCommand):
+
+
+    async def send_bot_help(self, mapping):
+        embed = discord.Embed(title ="Bot Help",
+            description = "A list of commands categorized. Use ! before commands",
+            colour = discord.Colour.gold())
+        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/968235354312290314/994066242988150874/brimwaddle.gif')
+        #'mapping' is a dict of the bot's cogs and their respective commands
+        for cog, cmds in mapping.items():
+            if cog is not None:
+                
+                list_of_commands = ""
+                for cmd in cmds:
+                    list_of_commands += f'{cmd.name}\n'
+                
+                embed.add_field(
+                    name = cog.qualified_name,
+                    value = list_of_commands,
+                    inline=False
+                )
+        embed.set_footer(text="Fluffy Inc.")
+        await self.get_destination().send(embed=embed)
+
+bot = commands.Bot(command_prefix="!", help_command= MyHelpCommand())
 
 #loads in an extension
 @bot.command()
@@ -23,6 +48,7 @@ async def reload(ctx, extension):
     bot.unload_extension(f'cogs.{extension}')
     bot.load_extension(f'cogs.{extension}')
 
+#searches and loads all cogs from the cogs directory
 for filename in os.listdir('./BunnyBot/cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
